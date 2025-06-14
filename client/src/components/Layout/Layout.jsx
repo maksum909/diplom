f1bd7023393c508c29ab09fcc7433f72663b03d6
Svirzhevskiy Maksum <129275173+maksum909@users.noreply.github.com>
2025-06-14 14:20:38 +0300

    try

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -27,7 +27,7 @@ const Layout = () => {
 
       const res = await getAccessTokenWithPopup({
         authorizationParams: {
-          audience: "http://localhost:8000",
+          audience: "https://homyz-estate.xyz/api",
           scope: "openid profile email",
         },
       });
