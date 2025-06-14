9e0b8593694079a0d7a8f75ebff7e53b7b1d87b6
Svirzhevskiy Maksum <129275173+maksum909@users.noreply.github.com>
2025-06-14 13:53:20 +0300

    try

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,10 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-n4h6byrcjwasuswo.us.auth0.com"
+      domain="dev-rg1xwvj44wl04jbz.us.auth0.com"
      //domain="http://localhost:8000"
      //clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
-      clientId="ZPjCNx5vEgkbX2YPTEk9hEcepRspuqVV"
+      clientId="TNMfEmtrj8v23fvfmqAMsBKNFT6dGSqV"
      authorizationParams={{
        //redirect_uri: "https://full-stack-real-estate-youtube-sooty.vercel.app"https://dev-n4h6byrcjwasuswo.us.auth0.com/api/v2/
       redirect_uri: "https://homyz-estate.xyz"
