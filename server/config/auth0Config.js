bb82838b1868bdc3f9454e6f3224be158a29a8f6
Svirzhevskiy Maksum <129275173+maksum909@users.noreply.github.com>
2025-06-20 22:24:31 +0300

    екн

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "https://estatora.online/api",
+    audience: "https://estatora.online",
     issuerBaseURL: "https://dev-rg1xwvj44wl04jbz.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
